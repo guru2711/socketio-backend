6e8ba539292ce9049c7bc59fbf9e41dc9845f9df
guru2711 <saiguruprakash@gmail.com>
2021-12-08 21:38:29 +0530

    "http"

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "https://socketiochatapp.netlify.app",
+    origin: "http://socketiochatapp.netlify.app",
     methods: ["GET", "POST"],
   },
 });
