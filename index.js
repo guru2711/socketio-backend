578166bcce2a81d3b8d5a0970013deba8b7c7201
guru2711 <saiguruprakash@gmail.com>
2021-12-08 21:03:33 +0530

    "site added in cors"

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://socketio-chata.netlify.app",
     methods: ["GET", "POST"],
   },
 });
